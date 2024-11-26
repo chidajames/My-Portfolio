fbaee582e5177b79af0b7c09f8362d04e66458dd
chidajames <chidanandgurudutt@gmail.com>
2024-11-26 22:49:17 +0000

    Updates

--- src/pages/Project.tsx
+++
@@ -1,9 +0,0 @@
-// This page will be implemented in phase 2 so a image is displayed when the user visits the projects page
-
-export default function Project() {
-    return (
-      <>
-      <img style={{width:"100%",height:"100%"}} src="https://media.istockphoto.com/id/1449580178/vector/under-construction-sign-vector-for-banner-website-ig.jpg?s=612x612&w=0&k=20&c=Eqa3KodRrtCGvMAd0Iay7K5bsNIH37zBbnLCLDPPARQ=" alt="" />
-      </>
-    );
-  }
