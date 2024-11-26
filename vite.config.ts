ef3fcf10a0932398e780379b72500e434fd2feb9
chidajames <chidanandgurudutt@gmail.com>
2024-11-26 22:50:52 +0000

    Vite.config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // Use the repository name as the base for GitHub Pages
   base: "./",
   plugins: [react()],
+  build: {
+    // Ensure proper file naming and chunking
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
